b70be7603ee8f58a70e555a4c37649ba62c297a4
ThanhTNV <thanhtran190304@gmail.com>
2024-12-26 11:58:01 +0700

    fix: remove /dist

--- dist/socket-io.adapter.d.ts
+++
@@ -1,7 +0,0 @@
-import { IoAdapter } from '@nestjs/platform-socket.io';
-import { INestApplication } from '@nestjs/common';
-import { ServerOptions } from 'socket.io';
-export declare class SocketIoAdapter extends IoAdapter {
-    constructor(app: INestApplication);
-    createIOServer(port: number, options?: ServerOptions): any;
-}
